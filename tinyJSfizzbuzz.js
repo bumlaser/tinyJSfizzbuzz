a3d4158134c4151e5ba88c10ff3b00fd21097e40
Nick Beatty <nick@mediamacros.net>
2016-02-10 22:47:24 -0500

    Better add some comments. All the cool kids are doing it.

--- tinyJSfizzbuzz.js
+++ tinyJSfizzbuzz.js
@@ -1,1 +1,14 @@
+/* 	
+
+	tinyJSfizzbuzz
+
+	* preincrement i to shorten the for loop
+	* An empty string ('') is considered false.
+	* Evaluate and assign the following: 
+		a) if i mod 3 is true (not zero) then '', else 'fizz'
+		b) if i mod 5 is true (not zero) then '', else 'buzz'
+	* log the result of a + b or, if false ('' + '') then log i.
+
+*/
+
 for(i=0;++i<101;)console.log((i%3?'':'fizz')+(i%5?'':'buzz')||i)
